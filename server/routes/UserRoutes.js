94774669b4be1f455d2bfd9c4ab0cbd57ac8d1e6
Vedant Gokhale <gokhalevedant06@gmail.com>
2022-01-21 20:34:45 +0530

    added auth middleware and jwt verify route

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const {signup,login} = require('../controllers/Users')
+const {signup,login,jwtVerify,uploadData} = require('../controllers/Users')
+const {isAuthenticated} = require('../middlewares/Auth')
 
 
 router.post("/signup", signup)
 router.post("/login", login)
+router.get("/jwtVerify", jwtVerify)
+router.get("/test", isAuthenticated)
+router.post("/uploadData",isAuthenticated,uploadData)
 
 
 module.exports = router;
