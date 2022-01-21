75566304537431839f9fff73181bf03dfbfd2137
shivam256 <shivamgavandi256@gmail.com>
2022-01-21 20:15:10 +0530

    boilerplate setup

--- client/src/index.js
+++ client/src/index.js
@@ -3,4 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/lib/integration/react";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate persistor={persistor} loading={<div>loading ...</div>}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
+);
