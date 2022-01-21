aaa4ce03fd24fbae96366007fe12c3564f014de3
AryanShinde <aryan.shinde20@vit.edu>
2022-01-21 22:39:54 +0530

    signup Page done

--- client/src/index.js
+++ client/src/index.js
@@ -7,12 +7,14 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/lib/integration/react";
-
+import { ChakraProvider } from "@chakra-ui/react";
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate persistor={persistor} loading={<div>loading ...</div>}>
       <BrowserRouter>
-        <App />
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
       </BrowserRouter>
     </PersistGate>
   </Provider>,
--- client/src/pages/sign-in/sign-in.componsnt.jsx
+++ client/src/pages/sign-in/sign-in.componsnt.jsx
@@ -1,7 +1,118 @@
-import React from 'react';
+import { Flex, Center, Spacer } from "@chakra-ui/layout";
+import { Box } from "@mui/system";
+import React from "react";
+import Signimg from "../../assets/images/Signimg.png";
+import { Input } from "@chakra-ui/input";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const SignIn = () => {
-  return <div> this is sign in page</div>;
+  return (
+    <Flex>
+      <ImageStyled className="left">
+        <img className="leftImg" src={Signimg} alt="" />
+      </ImageStyled>
+      <Flex
+        className="right"
+        justifyContent="center"
+        alignItems="center"
+        direction="column"
+        width="50%"
+      >
+        <Box
+          fontSize={{ base: "2.8rem", md: "2.8rem", sm: "1rem" }}
+          fontWeight="bold"
+          textAlign="center"
+          className="head"
+          p="1rem"
+        >
+          Create an account
+        </Box>
+        <form>
+          <label htmlFor="fname">First Name</label>
+          <br />
+          <Input
+            backgroundColor="#e6e6e6"
+            id="fname"
+            name="firstname"
+            type="text"
+            required
+            mb="1rem"
+            p="0.4rem"
+            w="20rem"
+            borderRadius="5px"
+          />
+          <br />
+          <label htmlFor="lname">Last Name</label>
+          <br />
+          <Input
+            mb="1rem"
+            p="0.4rem"
+            backgroundColor="#e6e6e6"
+            id="lname"
+            type="text"
+            w="20rem"
+            borderRadius="5px"
+            required
+          />
+          <br />
+          <label htmlFor="email">Email</label>
+          <br />
+          <Input
+            mb="1rem"
+            p="0.4rem"
+            backgroundColor="#e6e6e6"
+            id="email"
+            type="text"
+            w="20rem"
+            borderRadius="5px"
+            required
+          />
+          <br />
+          <label htmlFor="pass">Password</label>
+          <br />
+          <Input
+            mb="1rem"
+            p="0.4rem"
+            backgroundColor="#e6e6e6"
+            id="pass"
+            type="text"
+            w="20rem"
+            borderRadius="5px"
+            required
+          />
+          <br />
+          <label htmlFor="cpass">Confirm Password</label>
+          <br />
+          <Input
+            mb="1rem"
+            p="0.4rem"
+            backgroundColor="#e6e6e6"
+            id="cpass"
+            type="text"
+            w="20rem"
+            borderRadius="5px"
+            required
+          />
+          <br />
+        </form>
+        <Box p="2rem 0rem" className="already">
+          Already have an account ? <Link to="/">Login</Link>
+        </Box>
+      </Flex>
+    </Flex>
+  );
 };
+
+const ImageStyled = styled.div`
+  img {
+    width: 40rem;
+    height: 100vh;
+  }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
 
 export default SignIn;
