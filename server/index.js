ab7cc536e577e5a50b09afdc8274be7da2778429
Saharsh Samir <saharshsamir10@gmail.com>
2022-01-21 20:36:44 +0530

    added course routes

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
-const express = require('express')
+import express, { json } from 'express'
 const app = express()
-const dotenv = require('dotenv') 
-dotenv.config({path:'./config.env'}) 
+import { config } from 'dotenv' 
+config({path:'./config.env'}) 
 const port = process.env.PORT
 
 //using middleware to parse json data
-app.use(express.json());
+app.use(json());
 
-require('./db/conn');
+import './db/conn'
 
 app.use('/',require('./routes/UserRoutes'))
 app.use('/admin',require('./routes/AdminRoutes'))
+app.use('/course', require('./routes/CourseRoutes').default)
 
 app.listen(port, () => {
   console.log(`App listening on port http://localhost:${port}`)
---
+++ server/middlewares/IsTeacher.js
@@ -0,0 +1,9 @@
+
+
+export const isTeacher = (req, res, next) => {
+    if(req.user.isTeacher){
+        next();
+        return;
+    }
+    res.status(403).json({ok: false, error: "You need to be a teacher to access this route"});
+}
