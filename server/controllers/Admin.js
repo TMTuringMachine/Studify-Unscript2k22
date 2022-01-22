7cc5e5a494e3424f80f9d86cbb247cb7c18a0ae3
Vedant Gokhale <gokhalevedant06@gmail.com>
2022-01-22 17:31:35 +0530

    Admin APIs Done

--- server/controllers/Admin.js
+++ server/controllers/Admin.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcrypt");
 var jwt = require("jsonwebtoken");
 const Admin = require("../models/AdminSchema");
 const User = require("../models/UserSchema");
+const Course = require("../models/CourseSchema");
 
 const signup = async (req, res) => {
   var { name, email, phone, password, cpassword } = req.body;
@@ -65,7 +66,211 @@ const login = async (req, res) => {
   }
 };
 
+const getAllTeachers = async (req, res) => {
+  try {
+    const allTeachers = await User.find({ isTeacher: true });
+    res
+      .status(200)
+      .json({ ok: true, message: "Got All Teachers", allTeachers });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAllRequests = async (req, res) => {
+  try {
+    let reqArr = [];
+    const TeacherRequests = await User.find({ isTeacher: false });
+    for (const user of TeacherRequests) {
+      if (user.idProof) reqArr.push(user);
+    }
+    res.status(200).json({ ok: true, message: "Got All Requests", reqArr });
+  } catch (error) {
+    res.send(400).send(error);
+  }
+};
+
+const verifyTeacher = async (req, res) => {
+  try {
+    const { teacherID } = req.body;
+    const verified = await User.findByIdAndUpdate(teacherID, {
+      isTeacher: true,
+    });
+    res.status(200).json({ ok: true, message: "Teacher Verified", verified });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const declineRequest = async (req, res) => {
+  try {
+    const { teacherID } = req.body;
+    const declined = await User.findByIdAndUpdate(teacherID, {
+      isTeacher: false,
+      idProof: null,
+    });
+    res.status(200).json({ ok: true, message: "Teacher Declined", declined });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateTeacherDetails = async (req, res) => {
+  var {
+    teacherId,
+    name,
+    email,
+    phone,
+    password,
+    address,
+    age,
+    education,
+    image,
+    domain,
+    idProof,
+    rating,
+  } = req.body;
+  console.log(req.body);
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    password = hashedPassword;
+    const updatedTeacher = await User.findByIdAndUpdate(teacherId, {
+      name,
+      email,
+      phone,
+      password,
+      address,
+      age,
+      education,
+      image,
+      domain,
+      idProof,
+    });
+    res
+      .status(200)
+      .json({ ok: true, message: "Teacher Updated By Admin", updatedTeacher });
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+const removeTeacher = async (req, res) => {
+  try {
+    const { teacherID } = req.body;
+    const removeTeacher = await User.findByIdAndUpdate(teacherID, {
+      isTeacher: false,
+      idProof: null,
+    });
+    res
+      .status(200)
+      .json({ ok: true, message: "Teacher Removed", removeTeacher });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addCourse = async (req, res) => {
+  const {
+    title,
+    description,
+    thumbnail,
+    price,
+    content,
+    teacherId,
+    teacherName,
+    doubts,
+  } = req.body;
+
+  try {
+    let exists = await Course.findOne({ title: title });
+    if (exists) {
+      console.log(exists);
+      res.status(200).send("Course with this name already exists");
+    } else {
+      let newCourse = new Course({
+        title,
+        description,
+        thumbnail,
+        price,
+        content,
+        teacherId,
+        teacherName,
+        doubts,
+      });
+      let NewCourse = await newCourse.save();
+      return res
+        .status(200)
+        .json({ ok: true, message: "New Course Added by Admin", NewCourse });
+    }
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+const updateCourse = async (req, res) => {
+  const {
+    courseId,
+    title,
+    description,
+    thumbnail,
+    price,
+    content,
+    teacherId,
+    teacherName,
+    doubts,
+  } = req.body;
+  try {
+    const updatedCourse = await Course.findByIdAndUpdate(courseId, {
+      title,
+      description,
+      thumbnail,
+      price,
+      content,
+      teacherId,
+      teacherName,
+      doubts,
+    });
+    return res
+      .status(200)
+      .json({ ok: true, message: "Course Updated by Admin", updatedCourse });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteCourse = async (req, res) => {
+  const { courseId } = req.body;
+  try {
+    const Delete = await Course.findByIdAndDelete(courseId);
+    return res
+      .status(200)
+      .json({ ok: true, message: "Course Deleted by Admin", Delete });
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+const allCourses = async (req, res) => {
+  try {
+    let courses = await Course.find();
+    return res.status(200).json({ ok: true, message: "All Courses", courses });
+  } catch (error) {
+    res.send(error);
+  }
+};
+
 module.exports = {
   signup,
   login,
+  getAllTeachers,
+  getAllRequests,
+  verifyTeacher,
+  declineRequest,
+  removeTeacher,
+  addCourse,
+  updateCourse,
+  deleteCourse,
+  allCourses,
+  updateTeacherDetails,
 };
--- server/routes/AdminRoutes.js
+++ server/routes/AdminRoutes.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const router = express.Router();
-const { signup, login } = require("../controllers/Admin");
+const {
+  signup,
+  login,
+  getAllTeachers,
+  getAllRequests,
+  verifyTeacher,
+  declineRequest,
+  removeTeacher,
+  addCourse,
+  updateCourse,
+  deleteCourse,
+  allCourses,
+  updateTeacherDetails,
+} = require("../controllers/Admin");
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/allTeachers", getAllTeachers);
+router.get("/allTeacherRequests", getAllRequests);
+router.post("/verifyTeacher", verifyTeacher);
+router.post("/declineRequest", declineRequest);
+router.post("/removeTeacher", removeTeacher);
+router.post("/addCourse", addCourse);
+router.post("/updateCourse", updateCourse);
+router.post("/deleteCourse", deleteCourse);
+router.get("/allCourses", allCourses);
+router.post("/updateTeacher", updateTeacherDetails);
 
 module.exports = router;
