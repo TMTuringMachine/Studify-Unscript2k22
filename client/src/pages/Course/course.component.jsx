5728fcee028ba85b3f2442f1749b773f6f4304b1
shivam256 <shivamgavandi256@gmail.com>
2022-01-22 20:12:07 +0530

    course page

--- client/src/pages/Course/course.component.jsx
+++ client/src/pages/Course/course.component.jsx
@@ -1,19 +1,79 @@
-import React from 'react';
-import { SimpleGrid ,Box} from '@chakra-ui/react';
+import React from "react";
+import { SimpleGrid, Box, Text, Button, Flex, Spacer } from "@chakra-ui/react";
+import { Rating } from "@mui/material";
 
-import {CourseContainer,CourseImage} from './course.styles';
+import { CourseContainer, CourseImage } from "./course.styles";
+
+const course = {
+  thumbnail:
+    "https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80",
+  title: "Learning Blockchain By Building Your Own Javascript",
+  author: "Dr. A.B.C",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+};
 
 const Course = () => {
-  return <CourseContainer>
-      <SimpleGrid columns={2}>
+  return (
+    <CourseContainer
+      padding={["10px 5px 20px 5px","10px 5px 20px 5px","10px 5px 20px 5px" , "10px 30px 20px 30px"]}
+    >
+      <SimpleGrid columns={[1, null, 2, 2, 2]} marginTop="10px">
         <Box>
-            here will be the image
+          <CourseImage url={course.thumbnail} />
         </Box>
         <Box>
-            here will be the data
+          <Box
+            margin="0 auto"
+            height={["fit-content", "fit-content","fit-content", "35vh"]}
+            width="95%"
+            display="flex"
+            flexDirection="column"
+            justifyContent="space-between"
+          >
+            <div>
+              <Text fontSize={["2xl", "2xl", "3xl", "3xl", "4xl"]}>
+                {course.title}
+              </Text>
+              <Text margin={["0 0 10px 0",null]} fontSize={["md", "lg", "lg", "xl", "xl"]} color="#6d6d6d">
+                by {course.author}
+              </Text>
+            </div>
+            <Rating value={5} readOnly size="large" />
+            <Text fontSize={["3xl", "3xl", "4xl", "5xl", "5xl"]}>
+              {" "}
+              &#8377; 1000/-
+            </Text>
+          </Box>
+          <Flex width="95%" margin={["20px auto 0px auto","0 auto" ]}>
+            <Button
+              width="48%"
+              backgroundColor="#6C63FF"
+              _hover=""
+              color="#fff"
+            >
+              BUY NOW
+            </Button>
+            <Spacer />
+            <Button
+              width="48%"
+              variant="outline"
+              color="#6C63FF"
+              borderColor="#6C63FF"
+              borderWidth="2px"
+            >
+              ADD TO WISHLIST
+            </Button>
+          </Flex>
         </Box>
       </SimpleGrid>
-  </CourseContainer>;
+      <Box marginTop="30px" width="95%" margin="30px auto">
+        <Text fontSize="xl" color="#6d6d6d">
+          {course.description}
+        </Text>
+      </Box>
+    </CourseContainer>
+  );
 };
 
 export default Course;
--- client/src/pages/Course/course.styles.jsx
+++ client/src/pages/Course/course.styles.jsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 import { Box } from '@chakra-ui/react'
 
-export const CourseContainer = styled("div")(() => ({
+
+export const CourseContainer = styled(Box)(() => ({
   width: "100%",
   height: "100%",
   overflow: 'scroll',
-  padding:'10px 30px 20px 30px',
+//   padding:'10px 30px 20px 30px',
   // display:'flex',
   // flexDirection:'column',
   // alignItems:'center',
@@ -19,4 +20,22 @@ export const CourseImage = styled(Box)(({url})=>({
     backgroundPosition: "center",
     backgroundSize: "cover",
     backgroundColor:'red',
+    borderRadius:'10px',
+    margin:'0px auto 10px auto ',
 }))
+
+// export const CourseData = styled(Box)(({url})=>({
+//     width:'90%',
+//     '& .course-title':{
+//         fontSize:'2.2em',
+//         fontWeight:500
+//     },
+//     '& .course-author':{
+//         fontSize:'1.5em',
+//         color: '#636363',
+//     },
+//     '& .course-price':{
+//         fontSize:'3em',
+//         fontWeight:700
+//     }
+// }))
