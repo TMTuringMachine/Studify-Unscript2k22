5ca5aafc3b03ebb2047238eac2dfd9b26714c1c9
Saharsh Samir <saharshsamir10@gmail.com>
2024-06-30 18:35:05 +0530

    updated server url for frontend with deployed server

--- client/src/setUpProxy.js
+++ client/src/setUpProxy.js
@@ -1,16 +1,16 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
-
+const serverUrl = process.env.NODE_ENV === "development" ? "http://localhost:8080" : "https://studify.onrender.com";
 module.exports = function (app) {
   app.use(
     ["/admin", "/user", "/course"],
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: serverUrl,
     })
   );
   app.use(
     ["/user"],
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: serverUrl,
     })
   );
   // app.use(
--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const serverUrl = process.env.NODE_ENV === "development" ? "http://localhost:8080" : "https://studify.onrender.com";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: serverUrl,
 });
 
 export default axiosInstance;
